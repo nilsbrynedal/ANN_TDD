0f8ed6ca73bc000c755a2ef82b42e35e21c6a8e4
Nils <nils.brynedal@gmail.com>
2022-05-28 18:55:12 +0200

    Added test function that runs a small subset of the training data

--- ANN_TDD/ANN_TDD/Program.cs
+++ ANN_TDD/ANN_TDD/Program.cs
@@ -7,6 +7,9 @@ namespace ANN_TDD
     {
         static void Main()
         {
+            //Test();
+            //return;
+
             const int possibleAnswers = 10;
             float proportion;
             List<Data> data = Data.ReadFile();
@@ -14,7 +17,7 @@ namespace ANN_TDD
             (var trainingSet, var testingSet, var validationSet) = DataSplitter.Split(data, 0.7, 0.2);
 
             INetFactory factory = new NetFactory();
-            INet net = factory.CreateNet(data[0].Pixles.Length, possibleAnswers, 6, (float)0.1);
+            INet net = factory.CreateNet(data[0].Pixles.Length, possibleAnswers, 10, (float)0.04);
             IIdentifier identifier = new Identifier(net);
             ITrainer trainer = new Trainer();
             Tester tester = new Tester();
@@ -29,6 +32,40 @@ namespace ANN_TDD
 
             proportion = tester.Test(identifier, validationSet);
             Console.WriteLine("Identified " + (proportion * 100).ToString() + "% of validation set correctly");
+
+            Console.WriteLine("\nPress any key to close...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Test program to run on a small subset of the data
+        /// </summary>
+        private static void Test()
+        {
+            const int possibleAnswers = 10;
+            float proportion;
+            List<Data> data = Data.ReadFile();
+
+            Console.WriteLine("Testing with a very small part of the test data\n");
+
+            (var trainingSet, var testingSet, var validationSet) = DataSplitter.Split(data, 0.01, 0.2);
+
+            INetFactory factory = new NetFactory();
+            INet net = factory.CreateNet(data[0].Pixles.Length, possibleAnswers, 10, (float)0.01);
+            IIdentifier identifier = new Identifier(net);
+            ITrainer trainer = new Trainer();
+            Tester tester = new Tester();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                trainer.Train(net, trainingSet);
+                proportion = tester.Test(identifier, trainingSet);
+
+                Console.WriteLine(i.ToString() + "\tIdentified " + (proportion * 100).ToString() + "% of training set correctly");
+            }
+
+            proportion = tester.Test(identifier, trainingSet);
+            Console.WriteLine("Identified " + (proportion * 100).ToString() + "% of training set correctly");
 
             Console.WriteLine("\nPress any key to close...");
             Console.ReadKey();
